7ba9ec75e7f85b665b7b63470fbb9ffa4e8d49de
Faizefied393 <faiz.tariq57@gmail.com>
2025-03-09 07:30:15 -0500

    Implemented text viewing

--- editor.c
+++ editor.c
@@ -1,15 +1,29 @@
-#include <conio.h>
-#include <stdio.h>
+#include <fcntl.h>
 
-int main() {
-    char c;
-    printf("Press keys (press 'q' to quit):\n");
+void die(const char *s) {
+    perror(s);
+    exit(1);
+}
 
-    while (1) {
-        c = _getch();  // Reads a key without echoing it
-        if (c == 'q') break;
-        printf("You pressed: %c\n", c);
+void editorOpen(char *filename) {
+    int fd = open(filename, O_RDONLY);
+    if (fd == -1) die("open");
+
+    char buffer[1024];
+    int bytes_read = read(fd, buffer, sizeof(buffer));
+    if (bytes_read == -1) die("read");
+
+    write(STDOUT_FILENO, buffer, bytes_read);
+    close(fd);
+}
+
+int main(int argc, char *argv[]) {
+    enableRawMode();
+    if (argc >= 2) {
+        editorOpen(argv[1]);
+    } else {
+        printf("Usage: ./editor <filename>\n");
     }
-
     return 0;
 }
+
