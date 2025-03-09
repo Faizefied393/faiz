80c39ec330350b4150e1075e3dfec3f66f85f86a
Faizefied393 <faiz.tariq57@gmail.com>
2025-03-09 07:20:37 -0500

    Implemented raw mode

--- editor.c
+++ editor.c
@@ -1,15 +1,30 @@
-#include <conio.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <termios.h>
+#include <unistd.h>
+
+struct termios orig_termios;
+
+void disableRawMode() {
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
+void enableRawMode() {
+    tcgetattr(STDIN_FILENO, &orig_termios);
+    atexit(disableRawMode);
+
+    struct termios raw = orig_termios;
+    raw.c_lflag &= ~(ECHO | ICANON);
+
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
 
 int main() {
-    char c;
-    printf("Press keys (press 'q' to quit):\n");
+    enableRawMode();
+    printf("Raw mode enabled. Press 'q' to quit.\n");
 
-    while (1) {
-        c = _getch();  // Reads a key without echoing it
-        if (c == 'q') break;
-        printf("You pressed: %c\n", c);
-    }
+    char c;
+    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
 
     return 0;
 }
